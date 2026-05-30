6b4941e3f304afe4f9c0b8157d3259cb67436ae0
nataliia <natasyamorozova35@gmail.com>
2026-05-30 14:17:45 +0300

    Lab 4 Morozova AI-234

--- app/src/main/java/com/example/lab4_morozova_ai234/Screen.kt
+++ app/src/main/java/com/example/lab4_morozova_ai234/Screen.kt
@@ -4,6 +4,6 @@ sealed class Screen(val route: String) {
     object List : Screen("projects_list")
     object Add : Screen("add_project")
     object Details : Screen("details/{projectId}") {
-        fun createRoute(projectId: Int) = "details/$projectId" // Тепер приймає Int
+        fun createRoute(projectId: Int) = "details/$projectId"
     }
 }
